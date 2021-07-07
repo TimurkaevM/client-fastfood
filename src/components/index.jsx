a7c5fbd29b5b3071afe545faf9e0b9b1128ea7da
Muhammad <timurkaevmuhammad4@gmail.com>
2021-07-08 01:48:01 +0300

    delete test code in component App

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,32 +2,8 @@ import Header from '../components/Header/index';
 import Delivery from '../components/Delivery/index';
 import Main from '../components/Main/index';
 import Footer from '../components/Footer/index';
-import { useEffect } from 'react';
 
 function App() {
-  // useEffect(() => {}, []);
-
-  // const observer = new IntersectionObserver(
-  //   (entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         document.querySelectorAll('.main__category ').forEach((item) => {
-  //           item.classList.toggle(
-  //             'active',
-  //             item.getAttribute('data-name') === entry.target.id,
-  //           );
-  //         });
-  //       }
-  //     });
-  //   },
-  //   {
-  //     threshold: 0.7,
-  //   },
-  // );
-
-  // document
-  //   .querySelectorAll('data-sec')
-  //   .forEach((item) => observer.observe(item));
 
   return (
     <div className="App">
